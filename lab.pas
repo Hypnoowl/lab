3c50b12c82f08c1933b2b6ec40a845c4224cf5a7
Âëàäèñëàâ Íàçàðîâ <justsova2909@gmail.com>
2017-06-23 01:01:05 +0300

    dobavil raznost

--- lab.pas
+++ lab.pas
@@ -1,11 +1,13 @@
 var
 a,b:integer;
-c,d:integer;
+c,d,s:integer;
 begin
 readln(a);
 readln(b);
 c:=a+b;
 d:=a*2;
-writeln(c);
-writeln(d)
+s:=a-b;
+writeln('Сумма =',c);
+writeln('Произведение =',d);
+writeln('Разность =',s);
 end.
